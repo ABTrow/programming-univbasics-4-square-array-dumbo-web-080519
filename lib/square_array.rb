539465cd4beadffde18a0973445c33d075d15525
Austin Trow <austintrow@gmail.com>
2019-07-01 16:49:57 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,10 +1,10 @@
 def square_array(array)
   counter = 0
-  solution = []
+  
   while counter < array.length do
-    solution << array[counter] ** 2
+    array[counter] = array[counter] ** 2
     counter += 1
   end
   
-  solution
+  array
 end
